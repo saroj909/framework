d75c07cfa77acabedd62ba6850a949192a595bc4
saroj909 <sarojgochhayat3@gmail.com>
2023-06-14 20:36:37 +0530

    branch is added

--- src/practice/Demo.java
+++ src/practice/Demo.java
@@ -1,9 +1,12 @@
 package practice;
 
 public class Demo {
-public static void main(String[] args) {
+public static void main(String[] args)
+{
 	System.out.println("demo");
 	System.out.println("demo1");
 	System.out.println("hiiiiiiiiii");
+	System.out.println("helowww");
+	System.out.println("new branch ");
 }
 }
